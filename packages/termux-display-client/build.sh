8314a4695abfbe0e4c0f00b0b29bf4d0fdfb987d
chenjiaxin-max <jiaxinchen5712@gmail.com>
2025-01-29 21:24:21 +0800

    termux-display-client

---
+++ packages/termux-display-client/build.sh
@@ -0,0 +1,13 @@
+
+TERMUX_PKG_HOMEPAGE="https://github.com/jiaxinchen-max/termux-display-client"
+TERMUX_PKG_DESCRIPTION="An Android surface-based graphics rendering bridge that uses GLES and AHardwareBuff for acceleration. It helps software in the termux-package ecosystem perform cross-process graphics rendering."
+TERMUX_PKG_LICENSE="MPL-2.0"
+TERMUX_PKG_MAINTAINER="@chenjiaxin-max"
+TERMUX_PKG_VERSION="0.1.0"
+TERMUX_PKG_API_LEVEL="26"
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_SRCURL="http://127.0.0.1:8081/termux-packages/termux-display-client.tar.gz"
+TERMUX_PKG_SHA256=abe8974d01988f85cd95782cdbc480e9055cbbabe766cb1176ba79fdb276fad6
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+

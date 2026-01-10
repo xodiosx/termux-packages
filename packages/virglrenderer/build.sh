285ebed0dfad67e92b55fa292f17e454decd6d2f
Mody <mohammedaleiwi@gmail.com>
2026-01-10 15:49:50 +0300

    Update virglrenderer version and source URL

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -2,21 +2,19 @@ TERMUX_PKG_HOMEPAGE=https://virgil3d.github.io/
 TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="1.2.0"
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=b181b668afae817953c84635fac2dc4c2e5786c710b7d225ae215d15674a15c7
+TERMUX_PKG_VERSION=1.0.1.1
+TERMUX_PKG_SRCURL=https://github.com/xodiosx/virglrenderer-venus1.0/archive/refs/tags/1.0.1.1.tar.gz
+TERMUX_PKG_SHA256=9f8838fec3c025f1c2bcce12b3a9217a907a877c98dc017fde36e712b5088933
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx"
 
 termux_step_pre_configure() {
-	# error: using an array subscript expression within 'offsetof' is a Clang extension [-Werror,-Wgnu-offsetof-extensions]
-	# list_for_each_entry_safe(struct vrend_linked_shader_program, ent, &shader->programs, sl[shader->sel->type])
-	CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
+    # Disable Clang offsetof extension error
+    CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
 
-	if [[ $TERMUX_ARCH != "arm" ]]; then
-		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvenus=true"
-	fi
+    if [[ $TERMUX_ARCH != "arm" ]]; then
+        TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvenus=true"
+    fi
 }

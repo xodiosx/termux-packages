edfc5fa6c7d8e48d8d847b396f11fcdc96cb4160
Mody <mohammedaleiwi@gmail.com>
2026-01-10 15:54:22 +0300

    Fix formatting and update build script logic

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.0.1.1
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xodiosx/virglrenderer-venus1.0/archive/refs/tags/1.0.1.1.tar.gz
 TERMUX_PKG_SHA256=9f8838fec3c025f1c2bcce12b3a9217a907a877c98dc017fde36e712b5088933
 TERMUX_PKG_AUTO_UPDATE=true
@@ -11,10 +12,11 @@ TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx"
 
 termux_step_pre_configure() {
-    # Disable Clang offsetof extension error
-    CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
+	# Disable Clang offsetof extension error
+	CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
 
-    if [[ $TERMUX_ARCH != "arm" ]]; then
-        TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvenus=true"
-    fi
+	# Enable Venus only on non-arm architectures
+	if [[ $TERMUX_ARCH != "arm" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvenus=true"
+	fi
 }

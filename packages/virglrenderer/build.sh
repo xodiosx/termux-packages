ddc26bc55752c6093ea411e344f4e5d4802f6e1a
Mody <mohammedaleiwi@gmail.com>
2026-01-10 19:01:08 +0300

    Update virglrenderer package version and dependencies

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -2,21 +2,16 @@ TERMUX_PKG_HOMEPAGE=https://virgil3d.github.io/
 TERMUX_PKG_DESCRIPTION="A virtual 3D GPU for use inside qemu virtual machines"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.0.1.1
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://github.com/xodiosx/virglrenderer-venus1.0/archive/refs/tags/1.0.1.2.tar.gz
-TERMUX_PKG_SHA256=8a0d6b20c147d2a01603db3482f014ab9f99ce53eec40b0e19a9897bef3213e9
-TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa"
+TERMUX_PKG_VERSION="1.0.1"
+TERMUX_PKG_SRCURL=https://gitlab.freedesktop.org/virgl/virglrenderer/-/archive/virglrenderer-${TERMUX_PKG_VERSION}/virglrenderer-virglrenderer-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=446ab3e265f574ec598e77bdfbf0616ee3c77361f0574bec733ba4bac4df730a
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa, vulkan-loader"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx -Dvenus=true"
 
 termux_step_pre_configure() {
-	# Disable Clang offsetof extension error
+	# error: using an array subscript expression within 'offsetof' is a Clang extension [-Werror,-Wgnu-offsetof-extensions]
+	# list_for_each_entry_safe(struct vrend_linked_shader_program, ent, &shader->programs, sl[shader->sel->type])
 	CPPFLAGS+=" -Wno-error=gnu-offsetof-extensions"
-
-	# Enable Venus only on non-arm architectures
-	if [[ $TERMUX_ARCH != "arm" ]]; then
-		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvenus=true"
-	fi
 }

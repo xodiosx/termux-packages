653cca4ee2548aba8caf2acc43540e082f1f9d5d
Mody <mohammedaleiwi@gmail.com>
2026-01-10 15:57:46 +0300

    Disable automatic updates for virglrenderer package

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=1.0.1.1
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/xodiosx/virglrenderer-venus1.0/archive/refs/tags/1.0.1.1.tar.gz
 TERMUX_PKG_SHA256=9f8838fec3c025f1c2bcce12b3a9217a907a877c98dc017fde36e712b5088933
-TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libdrm, libepoxy, libglvnd, libx11, mesa"
 TERMUX_PKG_BUILD_DEPENDS="xorgproto"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="-Dplatforms=egl,glx"
